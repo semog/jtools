5f1da75fa053ecfda060395bddab240f6b2e4929
semog <semog@shelbyx.coliseum>
2016-04-07 16:10:45 -0700

    Fix Linux wrapper scripts to use function when using local variables.

--- cddx/cdd.sh
+++ cddx/cdd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-cddx.exe $*
-if [ $? -eq 1 ]; then
+function changedir()
+{
 	local cdscript="$HOME/.local/share/powertools/changedir.sh"
 	if [ -f $cdscript ]; then
 		. $cdscript
 		rm $cdscript
 	fi
+}
+
+cddx.exe $*
+if [ $? -eq 1 ]; then
+	changedir
 fi
--- pddx/pdd.sh
+++ pddx/pdd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-pddx.exe $*
-if [ $? -eq 1 ]; then
+function changedir()
+{
 	local cdscript="$HOME/.local/share/powertools/popdir.sh"
 	if [ -f $cdscript ]; then
 		. $cdscript
 		rm $cdscript
 	fi
+}
+
+pddx.exe $*
+if [ $? -eq 1 ]; then
+	changedir
 fi
